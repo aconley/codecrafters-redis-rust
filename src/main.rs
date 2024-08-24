0008bc44616ef5869a812d315c2af3331c87329e
Alex Conley <>
2024-08-23 21:16:02 -0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+const PONG: &[u8; 7] = b"+PONG\r\n";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                pong(stream).expect("Error handling connection");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn pong(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buf = [0u8; 256];
+    stream.read(&mut buf)?;
+    stream.write(PONG)?;
+    Ok(())
 }
