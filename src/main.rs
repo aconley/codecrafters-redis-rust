3cb706203f4c957f709502617adcdbf3cd86b268
Alex Conley <>
2024-08-25 18:32:08 -0600

    Rewrite to use tokio for event loop.
    Single threaded worker pool because that's how real Redis works.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use std::io::{Read, Write};
-use std::thread;
-use std::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
+const IP_PORT: &str = "127.0.0.1:6379";
 const PONG: &[u8; 7] = b"+PONG\r\n";
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+// Use only one worker thread so that the handler doesn't require locking.
+// This is like actual redis, which is single threaded on the state.
+#[tokio::main(worker_threads = 1)]
+async fn main() {
+    let listener = TcpListener::bind(IP_PORT).await.expect("Error connecting");
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await; 
         match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                thread::spawn(|| pong(stream).expect("Error handling connection"));
+            Ok((stream, addr)) => {
+                println!("accepted new connection from {}", addr);
+                tokio::spawn(async move { pong(stream).await.expect("Error handling message"); } );
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -20,14 +24,14 @@ fn main() {
     }
 }
 
-fn pong(mut stream: TcpStream) -> std::io::Result<()> {
+async fn pong(mut stream: TcpStream) -> std::io::Result<()> {
     let mut buf = [0u8; 256];
     loop {
-        let bytes_read = stream.read(&mut buf)?;
+        let bytes_read = stream.read(&mut buf).await?;
         if bytes_read == 0 {
             break;
         }
-        stream.write_all(PONG)?;
+        stream.write_all(PONG).await?;
     }
     Ok(())
 }
