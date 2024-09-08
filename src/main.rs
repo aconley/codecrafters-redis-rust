3073509402f34f8142e95006e2068f48a0c930f4
Alex Conley <>
2024-09-08 13:31:45 -0600

    Add clap command line parsing for CONFIG values.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod errors;
 mod resp_command;
 mod resp_parser;
 
+use clap::Parser;
+use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::net::TcpListener;
 
@@ -10,10 +12,31 @@ use crate::redis_handler::RedisHandler;
 
 const IP_PORT: &str = "127.0.0.1:6379";
 
+#[derive(Parser)]
+struct RedisArgs {
+    dir: Option<String>, 
+    dbfilename: Option<String>
+}
+
+impl RedisArgs {
+    fn to_config_dict(self) -> HashMap<String, String> {
+        let mut result = HashMap::new();
+        if let Some(dir) = self.dir {
+            result.insert("dir".to_string(), dir);
+        }
+        if let Some(dbfilename) = self.dbfilename {
+            result.insert("dbfilename".to_string(), dbfilename);
+        }
+        result
+    }
+}
+
 // Only use one worker thread to obey the contract of data_store::DataStore.
 #[tokio::main(worker_threads = 1)]
 async fn main() {
-    let handler = Arc::new(RedisHandler::new());
+    let args = RedisArgs::parse();
+    let handler = Arc::new(RedisHandler::new_with_config(args.to_config_dict()));
+
     let listener = TcpListener::bind(IP_PORT).await.expect("Error connecting");
 
     loop {
